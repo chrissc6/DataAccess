0434def42feb24d890d22f5d9a2f379bdb9b68a6
chris <christopherscaudill@gmail.com>
2020-06-05 07:58:55 -0400

    SQL Server pt6 Microsoft.Extensions.Configuration

--- DataAccessRelationalDB/SQLServerUI/Program.cs
+++ DataAccessRelationalDB/SQLServerUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace SQLServerUI
 {
@@ -6,7 +8,23 @@ namespace SQLServerUI
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine(GetConnectionString());
             Console.ReadLine();
+        }
+
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
         }
     }
 }
