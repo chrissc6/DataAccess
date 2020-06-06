e481df562e46058bcd527bb48017e7cdd5a091fc
chris <christopherscaudill@gmail.com>
2020-06-06 06:38:06 -0400

    SQLite pt5 System.Data.SQLite.Core

---
+++ DataAccessRelationalDB/DataAccessLibrary/SqlLiteDataAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Data.SQLite;
+using Dapper;
+using System.Linq;
+
+namespace DataAccessLibrary
+{
+    public class SqlLiteDataAccess
+    {
+        public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connString)
+        {
+            //create a connection to db with using statement
+            //using will always close out of connection properly 
+            using (IDbConnection connection = new SQLiteConnection(connString))
+            {
+                //once connection is created query connection
+                //Query<T> where we pass in the model of data
+                List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+
+                //list of rows in strongly typed models
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatement, T parameters, string connString)
+        {
+            using (IDbConnection connection = new SQLiteConnection(connString))
+            {
+                connection.Execute(sqlStatement, parameters);
+            }
+        }
+    }
+}
