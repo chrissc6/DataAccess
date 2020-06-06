d6af0363d2e4eab7c7e785425c531f5970744ab6
chris <christopherscaudill@gmail.com>
2020-06-06 07:17:59 -0400

    MySQL pt1

---
+++ DataAccessRelationalDB/MySQLUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySQLUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
