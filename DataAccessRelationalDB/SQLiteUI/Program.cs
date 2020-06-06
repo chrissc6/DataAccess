a41f81327587eeeac2d3fabe651d9d107dd6ee81
chris <christopherscaudill@gmail.com>
2020-06-06 06:21:20 -0400

    SQLite pt2

---
+++ DataAccessRelationalDB/SQLiteUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQLiteUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
