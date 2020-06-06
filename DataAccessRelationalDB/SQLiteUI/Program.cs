1d73be0f7594e53e781e71b206e82e6235e64124
chris <christopherscaudill@gmail.com>
2020-06-06 06:51:57 -0400

    SQLite pt8

--- DataAccessRelationalDB/SQLiteUI/Program.cs
+++ DataAccessRelationalDB/SQLiteUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
 
@@ -12,9 +14,9 @@ namespace SQLiteUI
             //Console.WriteLine(GetConnectionString());
 
             //instance of sqlcrud class, and pass in connstring
-            //SqlCrud sql = new SqlCrud(GetConnectionString());
+            SQLiteCrud sql = new SQLiteCrud(GetConnectionString());
 
-            //ReadAllContacts(sql);
+            ReadAllContacts(sql);
             //ReadContact(sql, 3);
             //CreateNewContact(sql);
             //UpdateContact(sql);
@@ -37,6 +39,59 @@ namespace SQLiteUI
             output = config.GetConnectionString(connectionStringName);
 
             return output;
+        }
+
+        private static void ReadAllContacts(SQLiteCrud sql)
+        {
+            //calls sqlcrud to get all contacts
+            var rows = sql.GetAllContacts();
+
+            foreach (var i in rows)
+            {
+                Console.WriteLine($"Id: {i.Id}, FirstName: {i.FirstName}, LastName: {i.LastName}");
+            }
+        }
+
+        private static void ReadContact(SQLiteCrud sql, int id)
+        {
+            var contact = sql.GetFullContactById(id);
+
+            Console.WriteLine($"Id: {contact.BasicInfo.Id}, FirstName: {contact.BasicInfo.FirstName}, LastName: {contact.BasicInfo.LastName}");
+        }
+
+        private static void CreateNewContact(SQLiteCrud sql)
+        {
+            FullContactModel nc = new FullContactModel
+            {
+                BasicInfo = new BasicContactModel
+                {
+                    FirstName = "ncFname",
+                    LastName = "ncLname"
+                }
+            };
+
+            nc.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "ncEmail@mail.com" });
+            nc.EmailAddresses.Add(new EmailAddressModel { Id = 2, EmailAddress = "test2@mail.com" });
+            nc.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-213-3333" });
+            nc.PhoneNumbers.Add(new PhoneNumberModel { Id = 1, PhoneNumber = "555-123-1111" });
+
+            sql.CreateContact(nc);
+        }
+
+        private static void UpdateContact(SQLiteCrud sql)
+        {
+            BasicContactModel contact = new BasicContactModel
+            {
+                Id = 1,
+                FirstName = "fName1z",
+                LastName = "lName1"
+            };
+            sql.UpdateContactName(contact);
+        }
+
+        private static void RemovePhoneNumberFromContact(SQLiteCrud sql, int contactId, int phoneNumberId)
+        {
+            sql.RemovePhoneNumber(contactId, phoneNumberId);
         }
     }
 }
