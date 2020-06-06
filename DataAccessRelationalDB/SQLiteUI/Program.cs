3d8cad6785c6d70706bb9d0aef910a77235c779d
chris <christopherscaudill@gmail.com>
2020-06-06 06:26:49 -0400

    SQLite pt3 Microsoft.Extensions.Configuration.Json

--- DataAccessRelationalDB/SQLiteUI/Program.cs
+++ DataAccessRelationalDB/SQLiteUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace SQLiteUI
 {
@@ -6,7 +8,35 @@ namespace SQLiteUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //test connstring
+            //Console.WriteLine(GetConnectionString());
+
+            //instance of sqlcrud class, and pass in connstring
+            //SqlCrud sql = new SqlCrud(GetConnectionString());
+
+            //ReadAllContacts(sql);
+            //ReadContact(sql, 3);
+            //CreateNewContact(sql);
+            //UpdateContact(sql);
+            //RemovePhoneNumberFromContact(sql, 1, 1);
+
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
         }
     }
 }
