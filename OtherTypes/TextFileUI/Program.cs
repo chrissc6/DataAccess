0a197d46a60836e741670ea740aeaf4c37fc7882
chris <christopherscaudill@gmail.com>
2020-06-07 09:17:45 -0400

    TextFile pt1

---
+++ OtherTypes/TextFileUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextFileUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
