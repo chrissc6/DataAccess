475ad43f46ab507c0c2f1e66168de86313d2d954
chris <christopherscaudill@gmail.com>
2020-06-07 09:11:35 -0400

    MongoDB pt10

--- NoSQLDB/MongoDBUI/Program.cs
+++ NoSQLDB/MongoDBUI/Program.cs
@@ -3,6 +3,7 @@ using DataAccessLibrary.Models;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace MongoDBUI
 {
@@ -25,7 +26,12 @@ namespace MongoDBUI
             nc.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-123-1111" });
             nc.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "555-123-2222" });
 
-            CreateContact(nc);
+            //CreateContact(nc);
+            //GetAllContacts();
+            //GetContactById("9c8e2f9f-15c1-411e-9246-a99908dcb5b5");
+            //UpdateContactsFirstName("nFname1z", "9c8e2f9f-15c1-411e-9246-a99908dcb5b5");
+            //RemovePhoneNumber("555-123-2222", "9c8e2f9f-15c1-411e-9246-a99908dcb5b5");
+            //RemoveContact("9c8e2f9f-15c1-411e-9246-a99908dcb5b5");
 
             Console.WriteLine("done mongo");
             Console.ReadLine();
@@ -49,6 +55,50 @@ namespace MongoDBUI
         private static void CreateContact(ContactModel contact)
         {
             db.UpsertRecord(tableName, contact.Id, contact);
+        }
+
+        private static void GetAllContacts()
+        {
+            var contacts = db.LoadRecords<ContactModel>(tableName);
+
+            foreach (var i in contacts)
+            {
+                Console.WriteLine($"Id: {i.Id}, FirstName: {i.FirstName}, LastName: {i.LastName}");
+            }
+        }
+
+        private static void GetContactById(string id)
+        {
+            Guid guid = new Guid(id);
+            var contact = db.LoadRecordById<ContactModel>(tableName, guid);
+
+            Console.WriteLine($"Id: {contact.Id}, FirstName: {contact.FirstName}, LastName: {contact.LastName}");
+        }
+
+        private static void UpdateContactsFirstName(string firstName, string id)
+        {
+            Guid guid = new Guid(id);
+            var contact = db.LoadRecordById<ContactModel>(tableName, guid);
+
+            contact.FirstName = firstName;
+
+            db.UpsertRecord(tableName, contact.Id, contact);
+        }
+
+        public static void RemovePhoneNumber(string phoneNumber, string id)
+        {
+            Guid guid = new Guid(id);
+            var contact = db.LoadRecordById<ContactModel>(tableName, guid);
+
+            contact.PhoneNumbers = contact.PhoneNumbers.Where(x => x.PhoneNumber != phoneNumber).ToList();
+
+            db.UpsertRecord(tableName, contact.Id, contact);
+        }
+
+        public static void RemoveContact(string id)
+        {
+            Guid guid = new Guid(id);
+            db.DeleteRecord<ContactModel>(tableName, guid);
         }
     }
 }
