e2f2e84826eaf4ffaa78d39c1512855ad297d03f
chris <christopherscaudill@gmail.com>
2020-06-07 07:57:07 -0400

    MongoDB pt1

---
+++ NoSQLDB/MongoDBUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoDBUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
