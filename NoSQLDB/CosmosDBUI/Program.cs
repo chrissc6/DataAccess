9764f9cc1645a65b86ce077fe279b5cad5e32452
chris <christopherscaudill@gmail.com>
2020-06-09 11:10:09 -0400

    CosmosDB pt1

---
+++ NoSQLDB/CosmosDBUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmosDBUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
