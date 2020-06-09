2d8b6b02f5b25964b245b69268bf9c2f3b026fdf
chris <christopherscaudill@gmail.com>
2020-06-09 09:47:12 -0400

    TextFile pt2 Microsoft.Extensions.Configuration.Json

--- OtherTypes/TextFileUI/Program.cs
+++ OtherTypes/TextFileUI/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Globalization;
+using System.IO;
 
 namespace TextFileUI
 {
     class Program
     {
+        private static IConfiguration _config;
+
         static void Main(string[] args)
         {
+            InitializeConfiguration();
+
             Console.WriteLine("Hello World!");
+        }
+
+        private static void InitializeConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            _config = builder.Build();
         }
     }
 }
