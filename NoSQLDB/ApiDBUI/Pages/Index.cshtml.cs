1854bbfbe0231f5a361c4e70241d696bd816d35c
chris <christopherscaudill@gmail.com>
2020-06-09 14:36:01 -0400

    API pt7

--- NoSQLDB/ApiDBUI/Pages/Index.cshtml.cs
+++ NoSQLDB/ApiDBUI/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,10 +12,12 @@ namespace ApiDBUI.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory httpClientFactory;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
+            this.httpClientFactory = httpClientFactory;
         }
 
         public void OnGet()
