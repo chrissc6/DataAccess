c1483e422d72d895aa599e2c911938fe593b8e48
chris <christopherscaudill@gmail.com>
2020-06-09 11:15:06 -0400

    CosmosDB pt2 Microsoft.Extensions.Configuration.Json Microsoft.Azure.Cosmos

--- NoSQLDB/CosmosDBUI/Program.cs
+++ NoSQLDB/CosmosDBUI/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace CosmosDBUI
 {
@@ -6,7 +9,47 @@ namespace CosmosDBUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("done cosmos");
+            Console.ReadLine();
+        }
+
+        private static string GetConnectionString(string connectionStringName = "Default")
+        {
+            string output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(connectionStringName);
+
+            return output;
+        }
+
+        private static void CreateContact(ContactModel contact)
+        {
+        }
+
+        private static void GetAllContacts()
+        {
+        }
+
+        private static void GetContactById(string id)
+        {
+        }
+
+        private static void UpdateContactsFirstName(string firstName, string id)
+        {
+        }
+
+        public static void RemovePhoneNumber(string phoneNumber, string id)
+        {
+        }
+
+        public static void RemoveContact(string id)
+        {
         }
     }
 }
