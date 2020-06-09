5cd1c3d8f551c124e2a9c8a2cd458be458ca2d6c
chris <christopherscaudill@gmail.com>
2020-06-09 10:29:38 -0400

    TextFile pt6

--- OtherTypes/TextFileUI/Program.cs
+++ OtherTypes/TextFileUI/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 
@@ -9,11 +12,34 @@ namespace TextFileUI
     {
         private static IConfiguration _config;
         private static string txtFile;
+        private static TextFileDataAccess db = new TextFileDataAccess();
 
         static void Main(string[] args)
         {
             InitializeConfiguration();
             txtFile = _config.GetValue<string>("TextFile");
+
+            ContactModel user1 = new ContactModel();
+            user1.FirstName = "fName1";
+            user1.LastName = "lName1";
+            user1.EmailAddresses.Add("emailA1@mail.com");
+            user1.EmailAddresses.Add("emailA2@mail.com");
+            user1.PhoneNumbers.Add("555-111-0001");
+            user1.PhoneNumbers.Add("555-111-0002");
+
+            ContactModel user2 = new ContactModel();
+            user2.FirstName = "fName2";
+            user2.LastName = "lName2";
+            user2.EmailAddresses.Add("emailB1@mail.com");
+            user2.EmailAddresses.Add("emailB2@mail.com");
+            user2.PhoneNumbers.Add("555-222-0001");
+            user2.PhoneNumbers.Add("555-222-0002");
+
+            List<ContactModel> contacts = new List<ContactModel>
+            {
+                user1,
+                user2
+            };
 
             Console.WriteLine("Hello World!");
         }
