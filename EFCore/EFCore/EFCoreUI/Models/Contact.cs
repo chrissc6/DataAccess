ce86e2af4e43ad20d2b94be3174a6c20c8326735
chris <christopherscaudill@gmail.com>
2020-06-14 15:48:21 -0400

    EFCore pt2

---
+++ EFCore/EFCore/EFCoreUI/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreUI.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Email> EmailAddresses { get; set; } = new List<Email>();
+        public List<Phone> PhoneNumbers { get; set; } = new List<Phone>();
+    }
+}
---
+++ EFCore/EFCore/EFCoreUI/Models/Phone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreUI.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
